25959f15688ac0669d8a871719c4b04c7f884b77
Brennedith Garcia <brennedithg@gmail.com>
2019-06-10 21:44:09 -0500

    Add index page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Route, Switch } from 'react-router';
 
+import Index from './pages/index';
 import Login from './pages/login';
 import Accounts from './pages/accounts';
 import Video from './pages/video';
@@ -12,6 +13,7 @@ function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/" component={Index} />
         <Route exact path="/login" component={Login} />
         <Route path="/accounts" component={Accounts} />
         <Route component={Video} />
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import FullHeight from './components/FullHeight';
+
+const index = () => {
+  return (
+    <FullHeight centered>
+      <a href="/login">
+        <h1 className="title is-1 has-text-link">Video Interview</h1>
+      </a>
+    </FullHeight>
+  );
+};
+
+export default index;
