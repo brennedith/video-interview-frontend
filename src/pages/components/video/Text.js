7fbdeefedeb3686386a4b90fde8b3240504b20ec
Brennedith Garcia <brennedithg@gmail.com>
2019-06-16 14:10:48 -0500

    The text is now splited in paragraphs

--- src/pages/components/video/Text.js
+++ src/pages/components/video/Text.js
@@ -3,11 +3,10 @@ import React from 'react';
 import './Text.css';
 
 const Text = ({ text }) => {
-  return (
-    <article className="text-container">
-      <p>{text}</p>
-    </article>
-  );
+  const formatedText = text ? text.split('\n') : [];
+  const paragraps = formatedText.map((p, i) => <p key={i}>{p}</p>);
+
+  return <article className="text-container">{paragraps}</article>;
 };
 
 export default Text;
