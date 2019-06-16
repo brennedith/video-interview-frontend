d41d64127ebeeb0bf1a2f44c7fb03f4f7061457d
Brennedith Garcia <brennedithg@gmail.com>
2019-06-16 14:10:15 -0500

    Refactor the way queries are passed to link

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,7 @@ import FullHeight from './components/FullHeight';
 const index = () => {
   const videoLinkQueries = {
     key: 'company-name-uuid',
-    text:
-      'El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña tocaba el saxofón detrás del palenque de paja.',
+    text: `El veloz murciélago hindú comía feliz cardillo y kiwi.\nLa cigüeña tocaba el saxofón detrás del palenque de paja.`,
     duration: 15,
     host: 'myftphost',
     user: 'user',
@@ -15,12 +14,12 @@ const index = () => {
   };
 
   const videoLink = Object.keys(videoLinkQueries).reduce((acc, q) => {
-    return acc + `${q}=${videoLinkQueries[q]}&`;
+    return acc + `${q}=${encodeURI(videoLinkQueries[q])}&`;
   }, '/video?');
 
   return (
     <FullHeight centered>
-      <a href={videoLink}>
+      <a href={videoLink} target="_blank" rel="noopener noreferrer">
         <h1 className="title is-1 has-text-link">Link</h1>
         <h2 className="subtitle is-2 has-text-success">{videoLink}</h2>
       </a>
